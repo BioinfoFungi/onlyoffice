ad0e8dfd044f15061ec35c57b50dbb519a73259e
wangyang <1749748955@qq.com>
2023-09-01 09:44:52 +0800

    update

---
+++ run.sh
@@ -0,0 +1,17 @@
+bash install-Debian.sh -it WORKSPACE -ls true
+
+wget https://download.onlyoffice.com/install/install-Debian/bootstrap.sh -O  install-Debian/bootstrap.sh
+
+
+wget https://download.onlyoffice.com/install/install-Debian/tools.sh -O  install-Debian/tools.sh
+wget https://download.onlyoffice.com/install/install-Debian/check-ports.sh -O install-Debian/check-ports.sh
+wget https://download.onlyoffice.com/install/install-Debian/install-preq.sh -O install-Debian/install-preq.sh
+wget https://download.onlyoffice.com/install/install-Debian/install-app.sh -O install-Debian/install-app.sh
+
+
+
+
+source install-Debian/tools.sh
+source install-Debian/check-ports.sh
+source install-Debian/install-preq.sh
+source install-Debian/install-app.sh
